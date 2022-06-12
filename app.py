326b3f419ba5a310fa2b9c3ad2e1ae78e3b1f9ac
zlliu246 <zlliu246@gmail.com>
2022-06-12 17:33:25 +0800

    added frontend

--- app.py
+++ app.py
@@ -5,6 +5,21 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/") # this is an endpoint -> is user come to this endpoint, this function return {message: ...}
 def home():
     return {"message": "hello world from app.py"}
